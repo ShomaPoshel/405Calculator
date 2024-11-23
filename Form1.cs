b3a1dc429e677cd859dac06cf0aa3f1868bbf4d6
Jam <danpushkov@yandex.ru>
2024-11-23 11:00:15 +0300

    FrontEnd

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Money_Calculator
+{
+    public partial class Form1 : Form
+    {
+        bool drag = false;
+        Point start_point = new Point(0, 0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e) { button1.BackColor = Color.Maroon; }
+
+        private void button1_MouseLeave(object sender, EventArgs e) { button1.BackColor = Color.FromArgb(64, 64, 64); }
+
+        private void button2_Click(object sender, EventArgs e) { this.WindowState = FormWindowState.Minimized; }
+
+        private void button1_Click(object sender, EventArgs e) { Application.Exit(); }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+    }
+}
